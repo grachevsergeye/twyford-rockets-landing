2baec61321cc57163c26d0efc90ddb7931550fa7
grachevsergeye <sergey_grachev_03@mail.ru>
2025-03-30 06:24:18 +0300

    completed

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,20 @@
+let lastScrollTop = 0;
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scroll Down - Hide Header
+        header.style.top = "-70px";
+    } else {
+        // Scroll Up - Show Header
+        header.style.top = "0";
+    }
+
+    lastScrollTop = scrollTop;
+});
+
 const initApp = () => {
    const hamburgerBtn = document.getElementById('hamburger-button')
    const mobileMenu = document.getElementById ('mobile-menu')
